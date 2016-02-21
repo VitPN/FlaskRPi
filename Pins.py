829ae11036c0cb1dd3c6cef799d4ea1ca62f0cb6
Vitaliy <vitaliy.pnk@gmail.com>
2016-02-21 14:54:21 +0200

    Added files via upload

---
+++ Pins.py
@@ -0,0 +1,44 @@
+import RPi.GPIO as GPIO
+
+# BCM to WiringPi pin numbers
+P0 = 17 # LED pin
+P1 = 18 # Button pin
+
+def Init():
+    GPIO.setwarnings(False) # suppress GPIO used message
+    GPIO.setmode(GPIO.BCM) # use BCM pin numbers
+    GPIO.setup(P0, GPIO.OUT) # set LED pin as output
+    # set button pin as input
+    # also use internal pull-up so we don't need external resistor
+    GPIO.setup(P1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def LEDon():
+    GPIO.output(P0, GPIO.HIGH)
+
+def LEDoff():
+    GPIO.output(P0, GPIO.LOW)
+
+def SetLED(state):
+    if state:
+        LEDon()
+    else:
+        LEDoff()
+
+# the "not" is used to reverse the state of the input
+# when pull-up is used, 1 is returned when the button is not pressed
+def ReadButton():
+    if not GPIO.input(P1):
+        return True
+    else:
+        return False
+
+# if not used as a module (standalone), run this test program 
+if __name__ == "__main__":
+    Init()
+    try:
+        while(True):
+            SetLED(ReadButton())
+    except:
+        #clean exit on CTRL-C
+        GPIO.cleanup()
+        quit()
---
+++ go.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import Pins
+
+app = Flask(__name__)
+
+# return index page when IP address of RPi is typed in the browser
+@app.route("/")
+def Index():
+    return render_template("index.html", uptime=GetUptime())
+
+# ajax GET call this function to set led state
+# depeding on the GET parameter sent
+@app.route("/_led")
+def _led():
+    state = request.args.get('state')
+    if state=="on":
+        Pins.LEDon()
+    else:
+        Pins.LEDoff()
+    return ""
+
+# ajax GET call this function periodically to read button state
+# the state is sent back as json data
+@app.route("/_button")
+def _button():
+    if Pins.ReadButton():
+        state = "pressed"
+    else:
+        state = "not pressed"
+    return jsonify(buttonState=state)
+
+
+# Help me plaese!
+# After submit message, adress in brouser - 192.168.1.38:8000/_change, is not  - 192.168.1.38:8000/. Help me plaese!
+@app.route("/_change", methods=['POST'])
+def _change():
+    if request.method == 'POST':
+        # Get the value from the submitted form
+        lcdText = request.form['lcd']
+        print "---Message is", lcdText
+
+        # Send the message to the LCD  
+        # lcd.clear()  
+        # lcd.message(lcdText)  
+    else:
+        lcdText = None
+    return jsonify(value=lcdText)        # - is not work
+	# return ""                          # - is not work 
+	# return redirect(url_for("Index"))  # - is not work 
+    # return render_template("index.html", value=lcdText) # - is not work
+
+
+def GetUptime():
+    # get uptime from the linux terminal command
+    from subprocess import check_output
+    output = check_output(["uptime"])
+    # return only uptime info
+    uptime = output[output.find("up"):output.find("user")-5]
+    return uptime
+    
+# run the webserver on standard port 80, requires sudo
+if __name__ == "__main__":
+    Pins.Init()
+    app.run(host='0.0.0.0', port=8000, debug=True)
+	
